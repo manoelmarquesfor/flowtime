70c3a0f612c87c52454590855d13c71d1bec304a
Manoel Marques <manoeljuniorsb@hotmail.com>
2026-06-24 17:04:27 -0300

    feat: adicionando migration para criar tabela de usuários e inserir admin

---
+++ internal/database/migrations/20260624132617_usuario.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+CREATE TABLE usuario (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    sessao_id UUID,
+    regra VARCHAR(50) NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+    dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_usuario_email ON usuario(email);
+CREATE INDEX idx_usuario_sessao_id ON usuario(sessao_id);
+
+INSERT INTO usuario (id, nome, email, password, regra, ativo) VALUES
+    ('439b7929-1906-44bb-b8af-158a7dd5d6de', 'Administrador', 'admin@admin.com', 'admin', 'ADMIN', TRUE);
+
+-- +goose Down
+DROP TABLE usuario;
