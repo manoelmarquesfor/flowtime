2ada3f422d11248a3ab5ddb8d8f14365be2f5d09
Manoel Marques <manoeljuniorsb@hotmail.com>
2026-06-24 22:52:23 -0300

    feat: alterar retorno do método Login para uuid.UUID e implementar lógica de autenticação

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -2,10 +2,12 @@ package auth
 
 import (
 	"context"
+	"errors"
 
 	"github/manoelmarquesfor/flowtime/internal/errs"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Service struct {
@@ -22,19 +24,47 @@ func (s *Service) Login(
 	ctx context.Context,
 	email string,
 	password string,
-) (string, error) {
+) (uuid.UUID, error) {
 	if email == "" || password == "" {
-		return "", errs.NewValidationError("Email e senha são obrigatórios")
+		return uuid.Nil, errs.NewValidationError("Email e senha são obrigatórios")
 	}
 
-	return uuid.NewString(), nil
+	usuario, err := s.repository.UsuarioByEmail(ctx, email)
+	if err != nil {
+		if errors.As(err, &errs.ErrNotFound) {
+			return uuid.Nil, errs.NewValidationError("Email ou senha incorreto")
+		}
+
+		return uuid.Nil, err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(usuario.Password), []byte(password))
+	if err != nil {
+		return uuid.Nil, errs.NewValidationError("Email ou senha incorreto")
+	}
+
+	if !usuario.Ativo {
+		return uuid.Nil, errs.NewValidationError("Usuário inativo")
+	}
+
+	sessaoID := uuid.New()
+
+	err = s.repository.SaveSessao(ctx, &sessaoID, usuario.ID)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return sessaoID, nil
 }
 
 func (s *Service) Logout(
 	ctx context.Context,
-	sessionID string,
+	sessionID uuid.UUID,
 ) error {
-	// remover sessão
+	err := s.repository.SaveSessao(ctx, nil, sessionID)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
